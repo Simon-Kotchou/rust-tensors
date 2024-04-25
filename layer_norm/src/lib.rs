76cf5e57cc3eafe61ba2fdfd1826e1d218b90fbc
Simon Kotchou <simonkotchou@mines.edu>
2024-04-25 05:07:18 -0700

    backward start

--- layer_norm/src/lib.rs
+++ layer_norm/src/lib.rs
@@ -154,6 +154,115 @@ pub fn layernorm_forward(
     }
 }
 
+pub fn layernorm_backward(
+    inp_grad: &mut [f32],
+    weight_grad: &mut [f32],
+    bias_grad: &mut [f32],
+    out_grad: &[f32],
+    inp: &[f32],
+    mean: &[f32],
+    rstd: &[f32],
+    weight: &[f32],
+    b: usize,
+    t: usize,
+    c: usize,
+) {
+    let c_simd = c - (c % 8);
+
+    for bt in 0..(b * t) {
+        let offset = bt * c;
+        let inp_bt = &inp[offset..offset + c];
+        let out_grad_bt = &out_grad[offset..offset + c];
+        let inp_grad_bt = &mut inp_grad[offset..offset + c];
+
+        let mean_val = mean[bt];
+        let rstd_val = rstd[bt];
+
+        let mut sum_grad: __m256 = unsafe { _mm256_setzero_ps() };
+        let mut sum_inp_grad: __m256 = unsafe { _mm256_setzero_ps() };
+
+        let mut i = 0;
+        while i < c_simd {
+            let out_grad_i: __m256 = unsafe { _mm256_loadu_ps(&out_grad_bt[i]) };
+            let inp_i: __m256 = unsafe { _mm256_loadu_ps(&inp_bt[i]) };
+            let weight_i: __m256 = unsafe { _mm256_loadu_ps(&weight[i]) };
+
+            sum_grad = unsafe { _mm256_add_ps(sum_grad, out_grad_i) };
+            sum_inp_grad = unsafe {
+                _mm256_fmadd_ps(
+                    out_grad_i,
+                    _mm256_sub_ps(inp_i, _mm256_set1_ps(mean_val)),
+                    sum_inp_grad,
+                )
+            };
+
+            let grad_i: __m256 = unsafe {
+                _mm256_mul_ps(out_grad_i, _mm256_set1_ps(rstd_val * rstd_val))
+            };
+            let inp_grad_i: __m256 = unsafe {
+                _mm256_fmadd_ps(grad_i, weight_i, _mm256_set1_ps(0.0))
+            };
+            unsafe { _mm256_storeu_ps(&mut inp_grad_bt[i], inp_grad_i) };
+
+            let weight_grad_i: __m256 = unsafe {
+                _mm256_mul_ps(grad_i, _mm256_sub_ps(inp_i, _mm256_set1_ps(mean_val)))
+            };
+            unsafe { _mm256_storeu_ps(&mut weight_grad[i], weight_grad_i) };
+
+            unsafe { _mm256_storeu_ps(&mut bias_grad[i], out_grad_i) };
+
+            i += 8;
+        }
+
+        let sum_grad_scalar = hsum_ps_avx(sum_grad);
+        let sum_inp_grad_scalar = hsum_ps_avx(sum_inp_grad);
+
+        for j in c_simd..c {
+            let out_grad_j = out_grad_bt[j];
+            let inp_j = inp_bt[j];
+
+            sum_grad_scalar += out_grad_j;
+            sum_inp_grad_scalar += out_grad_j * (inp_j - mean_val);
+
+            let grad_j = out_grad_j * rstd_val * rstd_val;
+            inp_grad_bt[j] = grad_j * weight[j];
+            weight_grad[j] = grad_j * (inp_j - mean_val);
+            bias_grad[j] = out_grad_j;
+        }
+
+        let factor = rstd_val * rstd_val / c as f32;
+        let mean_grad = -sum_grad_scalar * rstd_val / c as f32;
+
+        i = 0;
+        while i < c_simd {
+            let inp_grad_i: __m256 = unsafe { _mm256_loadu_ps(&inp_grad_bt[i]) };
+            let inp_i: __m256 = unsafe { _mm256_loadu_ps(&inp_bt[i]) };
+
+            let term1: __m256 = unsafe { _mm256_set1_ps(factor * sum_inp_grad_scalar) };
+            let term2: __m256 = unsafe {
+                _mm256_mul_ps(
+                    _mm256_set1_ps(factor),
+                    _mm256_sub_ps(inp_i, _mm256_set1_ps(mean_val)),
+                )
+            };
+            let term3: __m256 = unsafe { _mm256_set1_ps(mean_grad) };
+
+            let inp_grad_i: __m256 = unsafe {
+                _mm256_add_ps(inp_grad_i, _mm256_add_ps(term1, _mm256_add_ps(term2, term3)))
+            };
+            unsafe { _mm256_storeu_ps(&mut inp_grad_bt[i], inp_grad_i) };
+
+            i += 8;
+        }
+
+        for j in c_simd..c {
+            inp_grad_bt[j] += factor * sum_inp_grad_scalar
+                + factor * (inp_bt[j] - mean_val)
+                + mean_grad;
+        }
+    }
+}
+
 #[inline]
 fn hsum_ps_avx(v: __m256) -> f32 {
     unsafe {
