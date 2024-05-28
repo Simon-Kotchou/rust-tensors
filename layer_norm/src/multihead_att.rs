6a3131ddf0c37fe99051a04a953e00b5dc7ffe47
Simon Kotchou <simonkotchou@mines.edu>
2024-05-27 23:56:03 -0700

    speedup attent

--- layer_norm/src/multihead_att.rs
+++ layer_norm/src/multihead_att.rs
@@ -18,6 +18,11 @@ pub fn multihead_attention_forward(
     let head_dim = c / num_heads;
     let head_dim_simd = head_dim - (head_dim % 8);
 
+    let mut q_h = vec![0.0; b * t * c];
+    let mut k_h = vec![0.0; b * t * c];
+    let mut v_h = vec![0.0; b * t * c];
+    let mut out_h = vec![0.0; b * t * c];
+
     for bt in 0..(b * t) {
         let offset = bt * c;
         let q_bt = &q[offset..offset + c];
@@ -31,40 +36,56 @@ pub fn multihead_attention_forward(
             let wv_h = &wv[h * c * head_dim..(h + 1) * c * head_dim];
             let wo_h = &wo[h * head_dim * c..(h + 1) * head_dim * c];
 
-            let q_h = &mut [0.0; 1024];
-            let k_h = &mut [0.0; 1024];
-            let v_h = &mut [0.0; 1024];
-            let out_h = &mut [0.0; 1024];
+            let q_h_offset = bt * c + h * head_dim;
+            let k_h_offset = bt * c + h * head_dim;
+            let v_h_offset = bt * c + h * head_dim;
+            let out_h_offset = bt * c + h * head_dim;
 
-            linear_forward(q_h, q_bt, wq_h, &[0.0; 1024], 1, c, head_dim);
-            linear_forward(k_h, k_bt, wk_h, &[0.0; 1024], 1, c, head_dim);
-            linear_forward(v_h, v_bt, wv_h, &[0.0; 1024], 1, c, head_dim);
+            linear_forward(
+                &mut q_h[q_h_offset..q_h_offset + head_dim],
+                q_bt,
+                wq_h,
+                &[0.0; 1024],
+                1,
+                c,
+                head_dim,
+            );
+            linear_forward(
+                &mut k_h[k_h_offset..k_h_offset + head_dim],
+                k_bt,
+                wk_h,
+                &[0.0; 1024],
+                1,
+                c,
+                head_dim,
+            );
+            linear_forward(
+                &mut v_h[v_h_offset..v_h_offset + head_dim],
+                v_bt,
+                wv_h,
+                &[0.0; 1024],
+                1,
+                c,
+                head_dim,
+            );
 
-            let mut attn_scores = &mut [0.0; 1024];
-            let mut i = 0;
-            while i < head_dim_simd {
-                let q_wide = unsafe { _mm256_loadu_ps(&q_h[i]) };
-                let k_wide = unsafe { _mm256_loadu_ps(&k_h[i]) };
+            let mut attn_scores = vec![0.0; head_dim];
+            for i in (0..head_dim_simd).step_by(8) {
+                let q_wide = unsafe { _mm256_loadu_ps(&q_h[q_h_offset + i]) };
                 let mut sum = unsafe { _mm256_setzero_ps() };
-
-                for j in 0..head_dim_simd {
-                    let q_j = unsafe { _mm256_set1_ps(q_h[j]) };
-                    let k_j = unsafe { _mm256_loadu_ps(&k_h[j]) };
-                    let dot = unsafe { _mm256_dp_ps(q_j, k_j, 0xff) };
+                for j in (0..head_dim_simd).step_by(8) {
+                    let k_wide = unsafe { _mm256_loadu_ps(&k_h[k_h_offset + j]) };
+                    let dot = unsafe { _mm256_dp_ps(q_wide, k_wide, 0xff) };
                     sum = unsafe { _mm256_add_ps(sum, dot) };
                 }
-
                 unsafe { _mm256_storeu_ps(&mut attn_scores[i], sum) };
-
-                i += 8;
             }
-
-            for j in head_dim_simd..head_dim {
+            for i in head_dim_simd..head_dim {
                 let mut dot = 0.0;
-                for k in 0..head_dim {
-                    dot += q_h[k] * k_h[k];
+                for j in 0..head_dim {
+                    dot += q_h[q_h_offset + i] * k_h[k_h_offset + j];
                 }
-                attn_scores[j] = dot;
+                attn_scores[i] = dot;
             }
 
             // Softmax
@@ -77,14 +98,13 @@ pub fn multihead_attention_forward(
             for i in 0..head_dim {
                 let mut sum = 0.0;
                 for j in 0..head_dim {
-                    sum += exp_scores[j] * v_h[j];
+                    sum += exp_scores[j] * v_h[v_h_offset + j];
                 }
-                out_h[i] = sum;
+                out_h[out_h_offset + i] = sum;
             }
-
             linear_forward(
                 &mut out_bt[h * head_dim..(h + 1) * head_dim],
-                out_h,
+                &out_h[out_h_offset..out_h_offset + head_dim],
                 wo_h,
                 &[0.0; 1024],
                 1,
